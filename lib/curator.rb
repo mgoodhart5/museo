6eca3478cbdfa45604d8c5c09e8e1cb038e4dab9
Mary Goodhart <marya.grace5@gmail.com>
2018-10-31 09:42:55 -0600

    Passing tests and classes with methods for it 2

--- lib/curator.rb
+++ lib/curator.rb
@@ -10,4 +10,20 @@ class Curator
     @photographs << photograph
   end
 
+  def add_artist(artist)
+    @artists << artist
+  end
+
+  def find_artist_by_id(id)
+    @artists.find_all do |artist|
+      id == artist.id
+    end
+  end
+
+  def find_photograph_by_id(id)
+    @photographs.find_all do |photograph|
+      id == photograph.id
+    end
+  end
+  
 end
--- test/curator_test.rb
+++ test/curator_test.rb
@@ -3,6 +3,7 @@ require 'minitest/pride'
 require 'pry'
 require './lib/curator'
 require './lib/photograph'
+require './lib/artist'
 
 class CuratorTest < Minitest::Test
   def test_that_it_exists
@@ -60,7 +61,76 @@ class CuratorTest < Minitest::Test
     curator.add_photograph(photo_1)
     curator.add_photograph(photo_2)
 
+    assert_instance_of Photograph, curator.photographs[0]
     assert_equal "1", curator.photographs.first.id
     assert_equal "Rue Mouffetard, Paris (Boy with Bottles)", curator.photographs.first.name
+  end
+
+  def test_that_it_can_add_artists
+    curator = Curator.new
+    artist_1 = Artist.new({
+      id: "1",
+      name: "Henri Cartier-Bresson",
+      born: "1908",
+      died: "2004",
+      country: "France"
+    })
+    artist_2 = Artist.new({
+      id: "2",
+      name: "Ansel Adams",
+      born: "1902",
+      died: "1984",
+      country: "United States"
+    })
+    curator.add_artist(artist_1)
+    curator.add_artist(artist_2)
+
+    assert_instance_of Artist, curator.artists[0]
+    assert_equal 2, curator.artists.count
+    assert_equal "Henri Cartier-Bresson", curator.artists.first.name
+  end
+
+  def test_that_it_can_find_artist_by_id
+    curator = Curator.new
+    artist_1 = Artist.new({
+      id: "1",
+      name: "Henri Cartier-Bresson",
+      born: "1908",
+      died: "2004",
+      country: "France"
+    })
+    artist_2 = Artist.new({
+      id: "2",
+      name: "Ansel Adams",
+      born: "1902",
+      died: "1984",
+      country: "United States"
+    })
+    curator.add_artist(artist_1)
+    curator.add_artist(artist_2)
+    selection = curator.find_artist_by_id(artist_2.id)
+
+    assert_equal [artist_2], selection
+  end
+
+  def test_that_it_can_find_photograph_by_id
+    curator = Curator.new
+    photo_1 = Photograph.new({
+      id: "1",
+      name: "Rue Mouffetard, Paris (Boy with Bottles)",
+      artist_id: "1",
+      year: "1954"
+    })
+    photo_2 = Photograph.new({
+      id: "2",
+      name: "Moonrise, Hernandez",
+      artist_id: "2",
+      year: "1941"
+    })
+    curator.add_photograph(photo_1)
+    curator.add_photograph(photo_2)
+    selection = curator.find_photograph_by_id(photo_1.id)
+
+    assert_equal [photo_1], selection
   end
 end
